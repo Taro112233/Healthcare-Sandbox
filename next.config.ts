05800a9f50d209ca123ac04f656151426f2e8abf
Thanatouchth <thanatouchth@gmail.com>
2025-12-28 15:37:08 +0700

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+// next.config.ts
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
